8962cd344641a61ce705758ef0327aae91043ba9
张江英 <zhangjiangying@songguo7.com>
2022-03-30 19:21:15 +0800

    just test git

--- app03/app_auth.py
+++ app03/app_auth.py
@@ -1,6 +1,16 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 
+from app03.models import UserToken
+
 class MyAuthentication(BaseAuthentication):
     def authenticate(self, request):
-        pass
+        token = request.GET.get('token')
+        if token:
+            user_token = UserToken.objects.filter(token=token).first()
+            if user_token:
+                return user_token.user,token
+            else:
+                raise AuthenticationFailed('认证失败')
+        else:
+            raise AuthenticationFailed('请求中需要携带token')
